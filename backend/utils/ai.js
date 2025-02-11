b54079b41788ed73602e957e8184ca2e7b384442
JackSvensson <Jackens1997@gmail.com>
2025-02-11 13:04:10 +0100

    Added the backend folder and did a test api run

---
+++ backend/utils/ai.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "developer", content: "You are a helpful assistant." }],
+    model: "gpt-4o-mini",
+    store: true,
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
